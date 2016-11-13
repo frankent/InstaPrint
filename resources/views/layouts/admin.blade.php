9ad9d41745dfb344421a53cb2ab1febacb076670
Kiattirat Sujjapongse <kiattirat@sujjapongse.com>
2016-11-14 01:16:27 +0700

    udpate template rev2 - edit full hd - 2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,7 +46,7 @@
 		  <!-- mini logo for sidebar mini 50x50 pixels -->
 		  <span class="logo-mini"><b>A</b>LT</span>
 		  <!-- logo for regular state and mobile devices -->
-		  <span class="logo-lg"><b>Instaprint</b></span>
+		  <span class="logo-lg"><b>Feed Carousel</b></span>
 		</a>
 		<!-- Header Navbar: style can be found in header.less -->
 		<nav class="navbar navbar-static-top">
