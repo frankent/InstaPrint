63b00859738f76c53b742dc0772bebb3fa662d71
keittirat.sat <keittirat@gmail.com>
2018-02-04 01:19:45 +0700

    change endpoint

--- config/instagram.php
+++ config/instagram.php
@@ -3,8 +3,8 @@
 return [
     'apiKey'      => 'be3024178c0140178d355571ac43fc7c',
     'apiSecret'   => '023a875f1c7447078918fdf92f27326b',
-//    'apiCallback' => 'http://instaprint.alpha.trycatch.in.th/callback',
-    'apiCallback' => 'http://feed.proxy.trycatch.in.th/callback',
+    'apiCallback' => 'http://instaprint.cmrabbit.com/callback',
+//    'apiCallback' => 'http://feed.proxy.trycatch.in.th/callback',
 //    'apiCallback' => 'http://' . $_SERVER["SERVER_NAME"] . '/callback'
 //    'apiCallback' => 'http://nds-instaprint.dev/callback'
 ];
