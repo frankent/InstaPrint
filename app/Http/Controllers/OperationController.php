a662bea07ce02d811c99be22a3a0d0122959d8bc
Kiattirat Sujjapongse <kiattirat@sujjapongse.com>
2018-04-12 03:52:08 +0700

    change layout for instaxflim

--- app/Http/Controllers/OperationController.php
+++ app/Http/Controllers/OperationController.php
@@ -152,7 +152,7 @@ class OperationController extends Controller
                         // create image
                         $this->createImage([
                             'id' => $post['id'],
-                            'img_url' => $post['images']['standard_resolution']['url'],
+                            'img_url' => $post['images']['low_resolution']['url'],
                             'caption' => empty($post['caption']) ? null : array_get($post['caption'], 'text'),
                             'username' => $userProfile['graphql']['shortcode_media']['owner']['full_name'],
                             'profile_pic' => $userProfile['graphql']['shortcode_media']['owner']['profile_pic_url']
@@ -235,11 +235,14 @@ class OperationController extends Controller
 
         echo PHP_EOL . "Process Post Ids: {$id} by {$username}" . PHP_EOL;
 
-        echo "Getting Profile Picture" . PHP_EOL;
-        $profile_blob = file_get_contents($profile_pic, false, stream_context_create($arrContextOptions));
+//        echo "Getting Profile Picture" . PHP_EOL;
+//        $profile_blob = file_get_contents($profile_pic, false, stream_context_create($arrContextOptions));
 
         echo "Getting Post Picture" . PHP_EOL;
         $post_blob = file_get_contents($img_url, false, stream_context_create($arrContextOptions));
+
+        $logo_blob = file_get_contents(public_path('img/pk-logo-border.png'), false );
+        $bottom_text_blob = file_get_contents(public_path('img/bottom_text.png'), false );
 
         /**
          * Imagick operate
@@ -248,46 +251,58 @@ class OperationController extends Controller
         $frame = new Imagick();
         $frame->newimage(1200, 1800, "#ffffff");
 
-        $profile_imagick = new Imagick();
-        $profile_imagick->readimageblob($profile_blob);
-        $profile_imagick->resizeimage(250, 250, Imagick::FILTER_CATROM, 1);
+//        $profile_imagick = new Imagick();
+//        $profile_imagick->readimageblob($profile_blob);
+//        $profile_imagick->resizeimage(250, 250, Imagick::FILTER_CATROM, 1);
+
+        $logo_imagick = new Imagick();
+        $logo_imagick->readImageBlob($logo_blob);
+        $logo_imagick->resizeImage(320, 320, Imagick::FILTER_CATROM, 1);
+
+        $bottom_text_imagick = new Imagick();
+        $bottom_text_imagick->readImageBlob($bottom_text_blob);
+        $bottom_text_imagick->resizeImage(699, 239, Imagick::FILTER_CATROM, 1);
 
         $post_imagick = new Imagick();
         $post_imagick->readimageblob($post_blob);
         $dim = $post_imagick->getImageGeometry();
 
-        $post_imagick->scaleimage(1100, 1100, 1);
+        $post_imagick->scaleimage(1050, 1050, 1);
 
         $dim = $post_imagick->getImageGeometry();
 
-        $frame->compositeimage($profile_imagick, Imagick::COMPOSITE_DEFAULT, 50, 50);
+//        $frame->compositeimage($profile_imagick, Imagick::COMPOSITE_DEFAULT, 50, 50);
 
-        $offset_x = (1100 - $dim["width"]) / 2;
-        $offset_y = (1100 - $dim["height"]) / 2;
+//        $offset_x = (1050 - $dim["width"]) / 2;
+//        $offset_y = (1050 - $dim["height"]) / 2;
 
 
-        $frame->compositeimage($post_imagick, Imagick::COMPOSITE_DEFAULT, (50 + $offset_x), (350 + $offset_y));
+        $frame->compositeimage($post_imagick, Imagick::COMPOSITE_DEFAULT, 75, 75);
 
         $draw = new ImagickDraw();
         $draw->setfont($font);
-        $draw->setGravity(Imagick::GRAVITY_NORTHWEST);
-        $draw->setfontsize(70);
-        $draw->setfontstyle(2);
-        $draw->setfillcolor("#000000");
-        $draw->setfontweight(900);
-        $draw->setStrokeWidth(2);
-        $draw->setstrokecolor("#000000");
+//        $draw->setGravity(Imagick::GRAVITY_NORTHWEST);
+        $draw->setfontsize(30);
+//        $draw->setfontstyle(2);
+        $draw->setfillcolor("#555555");
+//        $draw->setfontweight(900);
+//        $draw->setStrokeWidth(2);
+//        $draw->setstrokecolor("#000000");
 
-        $frame->annotateimage($draw, 350, 100, 0, $username);
+        $frame->annotateimage($draw, 75, 1725, 0, "@{$username}");
+//        $frame->annotateimage($draw, 50, 1400, 0, "24 / 04 / 2018");
 
         /**
          * caption
          */
-        $caption_imagick = new Imagick();
-        $caption_imagick->setFont($font);
-        $caption_imagick->setPointSize(50);
-        $caption_imagick->newPseudoImage(1000, 250, "caption:{$caption}");
-        $frame->compositeimage($caption_imagick, Imagick::COMPOSITE_DEFAULT, 50, 1500);
+//        $caption_imagick = new Imagick();
+//        $caption_imagick->setFont($font);
+//        $caption_imagick->setPointSize(70);
+//
+//        $caption_imagick->newPseudoImage(1000, 250, "caption:#PK_Together");
+//        $caption_imagick->colorizeImage('rgb(249, 174, 189)',1, true);
+//
+//        $frame->compositeimage($caption_imagick, Imagick::COMPOSITE_DEFAULT, 50, 1175);
 
 //                $draw->setfontsize(50);
 //                $draw->setStrokeWidth(1);
@@ -297,12 +312,17 @@ class OperationController extends Controller
 //                $frame->annotateimage($draw, 50, 1550, 0, $caption);
 
 
+        $frame->compositeimage($logo_imagick, Imagick::COMPOSITE_DEFAULT, 440, 965);
+        $frame->compositeimage($bottom_text_imagick, Imagick::COMPOSITE_DEFAULT, 426, 1500);
+
         $frame->writeimage($save_path);
 
         $frame->destroy();
-        $caption_imagick->destroy();
+//        $caption_imagick->destroy();
         $post_imagick->destroy();
-        $profile_imagick->destroy();
+//        $profile_imagick->destroy();
+        $logo_imagick->destroy();
+        $bottom_text_imagick->destroy();
 
         echo "Save image to: {$save_path}" . PHP_EOL;
     }
